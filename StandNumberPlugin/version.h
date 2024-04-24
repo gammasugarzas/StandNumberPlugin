7d9b1183679204003a952bc2a8e18eddd6337236
Tomo <bohus.tamas@gmail.com>
2024-04-24 18:26:13 +0200

    version 1.0.1

--- StandNumberPlugin/version.h
+++ StandNumberPlugin/version.h
@@ -8,8 +8,8 @@ constexpr auto PLUGIN_AUTHOR = "Tamas Bohus";
 constexpr auto PLUGIN_COPYRIGHT = "GPL v3";
 constexpr auto GITHUB_LINK = "https://github.com/gammasugarzas/StandNumberPlugin";
 #ifdef _DEBUG
-#define VERSION_FILE_STR		"1.0.0 DEV"
+#define VERSION_FILE_STR		"1.0.1 DEV"
 #else
-#define VERSION_FILE_STR		"1.0.0"
+#define VERSION_FILE_STR		"1.0.1"
 #endif
 #endif // !COPYRIGHTS
